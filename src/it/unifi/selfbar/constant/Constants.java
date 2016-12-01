776ce6c00444513d22732b25d2bfee44d39670b5
sᴀʟᴠᴀᴛᴏʀᴇ ʙ <svt.busiello@gmail.com>
2016-12-01 11:19:42 +0100

    nothing special

--- src/it/unifi/selfbar/constant/Constants.java
+++ src/it/unifi/selfbar/constant/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 	public static final double ARABIC_VALUE = 1.50;
 	public static final double MILK_VALUE = 0.20;
 	public static final double CREAM_VALUE = 0.20;
-	public static final double MARTINI_VALUE = 3.20;
 	public static final double SODA_VALUE = 0.30;
+	public static final double MARTINI_VALUE = 3.20;
 	public static final double APPETIZER_VALUE = 0.05;
 }
