7389d3022bc635e14630edbb9a6df5f4dc4b6695
Matteo Mauro <matteomauro@iMac-di-Matteo.local>
2016-12-03 23:07:57 +0100

    no message

--- tests/it/unifi/selfbar/BillTest.java
+++ tests/it/unifi/selfbar/BillTest.java
@@ -1,21 +1,34 @@
 package it.unifi.selfbar;
 
 import static org.junit.Assert.*;
-
+import static it.unifi.selfbar.constant.Constants.*;
+import org.junit.Before;
 import org.junit.Test;
 
 import it.unifi.selfbar.client.Bill;
-import it.unifi.selfbar.client.Table;
-import it.unifi.selfbar.decorator.Milk;
-import it.unifi.selfbar.exception.OrderNotDecorableException;
-import it.unifi.selfbar.product.Arabic;
-import it.unifi.selfbar.product.Order;
+import it.unifi.selfbar.decorator.*;
+import it.unifi.selfbar.product.*;
 
 public class BillTest {
+	Bill bill;
+
+	@Before
+	public void init() {
+		bill = new Bill();
+	}
 
 	@Test
-	public void negativeTotal() {
-		Bill bill = new Bill();
+	public void getTotalTest() {
+		bill.addOrder(new Milk(new Milk(new Arabic())));
+		bill.addOrder(new Martini());
+		bill.addOrder(new Soda(new Martini()));
+		double expected = ARABIC_VALUE + MILK_VALUE * 2 + MARTINI_VALUE + SODA_VALUE + MARTINI_VALUE;
+		assertEquals(expected, bill.getTotal(), 0.001);
+
+	}
+
+	@Test
+	public void negativeTotalTest() {
 		assertTrue(bill.getTotal() >= 0);
 	}
 
