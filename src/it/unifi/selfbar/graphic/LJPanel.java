bab12b7b822df6d45dc99624ff7857a84d78242f
Salvatore <svt.busiello@gmail.com>
2016-12-12 02:23:16 +0100

    Implement Interface for default method
    Refresh() is now default method of CompomentInterface

--- src/it/unifi/selfbar/graphic/LJPanel.java
+++ src/it/unifi/selfbar/graphic/LJPanel.java
@@ -1,11 +1,14 @@
 package it.unifi.selfbar.graphic;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public abstract class LJPanel extends JPanel {
+public abstract class LJPanel extends JPanel implements ComponentInterface {
 	private GridBagConstraints gridBagContraints;
 	
 	public LJPanel() {
@@ -13,5 +16,20 @@ public abstract class LJPanel extends JPanel {
 		gridBagContraints = new GridBagConstraints();
 		}
 
-
+	public void add(JLabel lbl, Color color, int size){
+		prepareLabel( lbl, color, size);
+		this.add(lbl);
+	}
+	
+	public void prepareLabel(JLabel lbl, Color color, int size){
+		lbl.setForeground(color);
+		lbl.setFont(new Font("Courier", Font.PLAIN , size));
+		add(lbl);
+	}
+	
+	public void refresh() {
+		this.repaint();
+		this.validate();
+	}
+	
 }
