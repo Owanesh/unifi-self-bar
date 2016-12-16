cfc85c3ba3afcd1e9c17dd5497bc13d871d9a01b
Salvatore <svt.busiello@gmail.com>
2016-12-16 12:36:12 +0100

    New Switch Method
    New switch method, by JLPanel title

--- src/it/unifi/selfbar/graphic/GUIController.java
+++ src/it/unifi/selfbar/graphic/GUIController.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 import it.unifi.selfbar.constant.Constants;
 import it.unifi.selfbar.constant.GraphicGuide;
 import it.unifi.selfbar.constant.ViewSets;
+import it.unifi.selfbar.exception.ErrorMessages;
 import it.unifi.selfbar.graphic.view.WelcomeView;
 
 import java.awt.*;
@@ -16,8 +17,7 @@ public class GUIController extends JFrame implements ComponentInterface{
 	/* Fields */
 	JPanel oldPnl;
 	JPanel currentPnl;
-	private HashMap viewList;
-	
+	private ViewSets viewManager = new ViewSets();
 	private static GUIController instance = null;
 	
 	/* Singleton */
@@ -28,32 +28,28 @@ public class GUIController extends JFrame implements ComponentInterface{
 	      return instance;
 	   }
 	
+ 	/* Constructor */
 	protected GUIController() {
 		super();
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setPreferredSize(new Dimension(GraphicGuide.WINDOW_WIDTH, GraphicGuide.WINDOW_HEIGHT));
-		configureBasicLayout();
-		this.add(new WelcomeView());
+		configureBasicLayout(); //look and feel by system
+		switchTo(GraphicGuide.WELCOME_VIEW); //initial view
 		refresh();
 		this.setVisible(true);
 	} 
 	
-	public void setCurrentPanel(JPanel newPnl) {
+	public void setCurrentPanel(JPanel newPanel) {
+		if(oldPnl!=null)
+			this.remove(oldPnl);
 		this.oldPnl = this.currentPnl;
-		this.currentPnl = newPnl;
+		this.currentPnl = newPanel;
+  		this.add(currentPnl);
 		this.refresh();
 	}
-
-	public JPanel getLastPanel() {
-		return oldPnl;
-	}
-
-	public JPanel getCurrentPanel() {
-		return currentPnl;
-	}
-
+	
 	public void resetLastPanel() {
-		JPanel temp = currentPnl;
+ 		JPanel temp = currentPnl;
 		currentPnl = oldPnl;
 		oldPnl = temp;
 		this.refresh();
@@ -65,6 +61,39 @@ public class GUIController extends JFrame implements ComponentInterface{
 		this.validate();
 	}
 
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	private String sanitizeString(String key){
+		key = key.toLowerCase();
+		key = key.replace(" ",""); //remove spaces
+		key = key.trim();
+		return key;
+	}
+	
+	/**
+	 * This method, Allows rapid switch across panels. Transition, using titleName of Destination
+	 */
+	public void switchTo(String key)  throws IllegalArgumentException {
+		key = sanitizeString(key);
+		if(viewManager.getView().containsKey(key)){
+			setCurrentPanel((LJPanel)viewManager.getView().get(key));
+		}
+		else{
+			throw new IllegalArgumentException(ErrorMessages.INVALID_VIEWKEY);
+		}
+	}
+	
+	
+	
+	/**
+	 * This method, set default LookAndFeel (from system)
+	 */
 	private void configureBasicLayout() {
 		getContentPane().setBackground(GraphicGuide.BACKGROUND_TONE);
 		this.setTitle(Constants.APPLICATION_NAME);
@@ -77,16 +106,6 @@ public class GUIController extends JFrame implements ComponentInterface{
 		}
 
 	}
-
-	@Override
-	public void reset() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	private void defineViewSet(){
-		this.viewList = ViewSets.map();
-	}
-	
+ 
  
 }
