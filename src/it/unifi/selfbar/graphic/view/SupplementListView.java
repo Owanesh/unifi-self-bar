0e9f8eef681ae02ee5fa9d454b29dafe5ec74277
Salvatore <svt.busiello@gmail.com>
2016-12-16 12:36:25 +0100

    View Element

---
+++ src/it/unifi/selfbar/graphic/view/SupplementListView.java
@@ -0,0 +1,23 @@
+package it.unifi.selfbar.graphic.view;
+
+import it.unifi.selfbar.graphic.LJPanel;
+
+public class SupplementListView extends LJPanel {
+
+	public SupplementListView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void goTo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
