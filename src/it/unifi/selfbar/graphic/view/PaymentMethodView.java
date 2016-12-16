c0c951911e1489cd93eaa881b10ac4a5af442dea
Salvatore <svt.busiello@gmail.com>
2016-12-16 12:36:25 +0100

    View Element

---
+++ src/it/unifi/selfbar/graphic/view/PaymentMethodView.java
@@ -0,0 +1,23 @@
+package it.unifi.selfbar.graphic.view;
+
+import it.unifi.selfbar.graphic.LJPanel;
+
+public class PaymentMethodView extends LJPanel {
+
+	public PaymentMethodView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void goTo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/it/unifi/selfbar/graphic/view/SelectView.java
@@ -0,0 +1,23 @@
+package it.unifi.selfbar.graphic.view;
+
+import it.unifi.selfbar.graphic.LJPanel;
+
+public class SelectView extends LJPanel {
+
+	public SelectView() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void goTo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
