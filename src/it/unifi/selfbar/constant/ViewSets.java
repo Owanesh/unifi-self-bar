4f135ca58a432ed3a5da2a3e784df11869801a10
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2016-12-21 13:10:40 +0100

    Removed double Hash for product

--- src/it/unifi/selfbar/constant/ViewSets.java
+++ src/it/unifi/selfbar/constant/ViewSets.java
@@ -10,8 +10,7 @@ import it.unifi.selfbar.order.*;
 public class ViewSets {
 
 	private static HashMap<String, LJPanel> view;
-	private static HashMap<String, Class> coffeeProduct;
-	private static HashMap<String, Class> cocktailProduct;
+	private static HashMap<String, Class> product;
 
 	public static HashMap getView() {
 		if (view == null) {
@@ -20,32 +19,20 @@ public class ViewSets {
 		return view;
 	}
 
-	public static HashMap getCoffeeProduct() {
-		if (coffeeProduct == null) {
-			coffeeProduct = initializeCoffeeProducts();
+	public static HashMap getProduct() {
+		if (product == null) {
+			product = initializeProducts();
 		}
-		return coffeeProduct;
+		return product;
 	}
 
-	public static HashMap getCocktailProduct() {
-		if (cocktailProduct == null) {
-			cocktailProduct = initializeCocktailProducts();
-		}
-		return cocktailProduct;
-	}
-
-	private static HashMap<String, Class> initializeCoffeeProducts() {
-		coffeeProduct = new HashMap<>();
-		coffeeProduct.put(Arabic.class.getSimpleName(), Arabic.class.getClass());
-		coffeeProduct.put(Brasilian.class.getSimpleName(), Brasilian.class.getClass());
- 		return coffeeProduct;
-	}
-
-	private static HashMap<String, Class> initializeCocktailProducts() {
-		cocktailProduct = new HashMap<>();
-		cocktailProduct.put(Martini.class.getSimpleName(), Martini.class.getClass());
-		cocktailProduct.put(Tequila.class.getSimpleName(), Tequila.class.getClass());
-		return cocktailProduct;
+	private static HashMap<String, Class> initializeProducts() {
+		product = new HashMap<>();
+		product.put(Arabic.class.getSimpleName(), Arabic.class.getClass());
+		product.put(Brasilian.class.getSimpleName(), Brasilian.class.getClass());
+		product.put(Martini.class.getSimpleName(), Martini.class.getClass());
+		product.put(Tequila.class.getSimpleName(), Tequila.class.getClass());
+		return product;
 	}
 
 	private static HashMap<String, LJPanel> initializeViews() {
